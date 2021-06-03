fa5a74631964cdb41d27634dbdb25a07ba244a28
Sayantan Basu <sayantanbasu@live.in>
2021-06-03 14:15:40 -0400

    it works

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
 const args = process.argv.splice(2);
+const fs = require('fs');
  let URL = args[0];
  let fp = args[1];
  console.log(URL);
  console.log(fp);
- request(URL,(error,response,body)=>{
-   console.log('error:', error); // Print the error if one occurred
-   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-   console.log('body:', body); // Print the HTML for the Google homepage.
+
+ request(URL,(error,response, body) =>{
+   let byteCounter = body.length;
+   fs.writeFile(URL,fp,()=>{
+    console.log (`downloaded ${byteCounter} bytes & saved the file to ${fp}`);
+   })
  })
+ 
+ 
+ 
